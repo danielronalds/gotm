002008568bccdcefdd47c05fdee0f1533bfaef18
Daniel R <nzdanron@gmail.com>
2025-02-25 00:34:40 +1300

    chore: update help menu

--- controllers/help.go
+++ controllers/help.go
@@ -16,12 +16,13 @@ func (c HelpController) HandleCmd(args []string) error {
 		fmt.Fprintf(os.Stderr, "\"%v\" is an unknown command\n\n", args[0])
 	}
 
-	help := `gotm v0.0.1
+	help := `gotm v0.0.2
 
 A cli tool building opinionated full stack web applications with the GOTM stack
 
 Commands
   new         Creates a new project with the passed in name
+  add         Adds a component to the project [controller, service, repository, view, model]
   help        Show this menu
 `
 
